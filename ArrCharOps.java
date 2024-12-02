07300193b828c1529c283fbd794317f56bcc5582
yameli2 <yam.elimelech@post.runi.ac.il>
2024-12-03 00:06:54 +0200

    q1

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -169,9 +169,14 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        for(int i=0;i<str1.length();i++){
+        int length=0;
+        if(str1.length()<str2.length())
+        length= str1.length();
+        else length= str2.length();
+        for(int i=0;i<length;i++){
             if(str1.charAt(i)!=str2.charAt(i))
             {
+
                 if(str1.charAt(i)<str2.charAt(i))
                 return -1;
                 if(str1.charAt(i)<str2.charAt(i))
@@ -179,7 +184,7 @@ public class ArrCharOps {
             } 
         }
         if(str1.length()<str2.length())
-        return -1;
+        return -1; 
         if(str2.length()<str1.length())
         return -2;
         else 
