bfe5b78523075dd59cddafe5a80e7473f1c7f2ec
yameli2 <yam.elimelech@post.runi.ac.il>
2024-12-03 14:23:04 +0200

    q2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,56 @@
 public class Primes {
+    static int Pcounnter;
+    
     public static void main(String[] args) {
-        // Replace this statement with your code
+      int n = Integer.parseInt(args[0]);
+     /// double p=((double)(CountPrime(n)/n))*100.0;
+      System.out.println("There are "+CountPrime(n)+" primes between 2 and "+n+". ("+((CountPrime(n)*100)/30)+"% are primes)");
+ }
+
+    public static boolean Isprime(int num){
+        if(num<=1)
+        return false;
+        for(int i =1;i<(int)Math.sqrt(num);i++)
+        {
+            if(num%i==0)
+            return false;
+        }
+        return true;
     }
-}
+
+    public static int CountPrime(int n){
+        Pcounnter=0;
+        int k =2;
+        boolean[] isprime= new boolean[n+1];
+        for(int i=2;i<n;i++){
+            isprime[i]=true;
+        }
+        for(int j=2;j<n;j++){
+            while (k<n) {
+                if(isprime[k]==true)
+                if(k%j==0&&k!=j)
+                isprime[k]=false;
+                k++;
+        }
+        k=j+1;    
+        }
+          //  for(int j=2;j<Math.sqrt(n);j++){
+            //    if (isprime[j]== true){
+              //  if(Isprime(j)==true)
+               // k=j+1;
+               // while(k<n){
+                //    if(k%j==0)
+                //    isprime[k]=false;
+                 //   k++;
+               // }
+               // }
+          //  }
+       // }
+           for(int t=0;t<isprime.length;t++){
+            if (isprime[t]== true)
+            Pcounnter++;
+           }
+            return Pcounnter;   
+        }
+        
+    }
