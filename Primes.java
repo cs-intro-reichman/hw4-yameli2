aede7fb1de80f9faab35dee237633fd3b267d893
yameli2 <yam.elimelech@post.runi.ac.il>
2024-12-03 17:59:21 +0200

    q2

--- Primes.java
+++ Primes.java
@@ -4,7 +4,7 @@ public class Primes {
     public static void main(String[] args) {
       int n = Integer.parseInt(args[0]);
       System.out.println("Prime numbers up to "+n+":");
-      System.out.println("There are "+CountPrime(n)+" primes between 2 and "+n+" ("+((CountPrime(n)*100)/n)+"% are primes)");
+      System.out.println("There are "+CountPrime(n)+" primes between 2 and "+n+" ("+((Pcounnter*100)/n)+"% are primes)");
  }
 
     public static int CountPrime(int n){
@@ -23,24 +23,14 @@ public class Primes {
         }
         k=j+1;    
         }
-          //  for(int j=2;j<Math.sqrt(n);j++){
-            //    if (isprime[j]== true){
-              //  if(Isprime(j)==true)
-               // k=j+1;
-               // while(k<n){
-                //    if(k%j==0)
-                //    isprime[k]=false;
-                 //   k++;
-               // }
-               // }
-          //  }
-       // }
+          
            for(int t=2;t<isprime.length;t++){
-            if (isprime[t]== true)
-            Pcounnter++;
-            System.out.println(t);
+            if (isprime[t]== true){
+                Pcounnter++;
+                System.out.println(t);
+            }
            }
-            return Pcounnter;   
+           return Pcounnter;   
         }
         
     }
