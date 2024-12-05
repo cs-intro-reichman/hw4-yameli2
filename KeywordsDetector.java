567779bb1ec72443fe5a76c591730baaa5a123ea
yameli2 <yam.elimelech@post.runi.ac.il>
2024-12-05 15:49:46 +0200

    q4

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -17,10 +17,48 @@ public class KeywordsDetector {
         String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
         detectAndPrint(sentences, keywords);
     }
+       
+        public static boolean contains(String str1, String str2) {
+             str1=str1.toLowerCase();
+             str2=str2.toLowerCase();
+             if(str1.length()<str2.length())
+             return false;
+             for(int i =0;i<str1.length();i++)
+             {
+                 boolean equal=true;
+                 for(int j=0;j<str2.length();j++){
+                     if(str1.charAt(j+i)!= str2.charAt(j))
+                     {
+                         equal=false;
+                        break;
+                     }
+                 }
+                 if (equal) {
+                     return true;
+                     
+                 } 
+     
+             }
+             return false;
+         }
+       
+
+
 
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         // Replace this comment with your code
+        for(int i =0;i<sentences.length;i++){
+            for(int j=0;j<keywords.length;j++){
+                String str=keywords[j];
+                if(sentences[i].contains(str))
+                {
+                    System.out.println(sentences[i]);
+                    break;
+                }    
+            }
+           
+        }
     }
 }
